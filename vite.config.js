7ee00250ccde6f317872a3878f510e62568f9f2b
Charles Wilfried KOUADIO <kcharly_91@hotmail.com>
2023-08-04 15:09:14 +0000

    Laravel/octane with swoole configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { homedir } from 'os'
+import { resolve } from 'path'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+
+let host = 'djera-workflows.test'
 
 export default defineConfig({
+    server: detectServerConfig(host),
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -18,3 +24,28 @@ export default defineConfig({
         }),
     ],
 })
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(
+        homedir(),
+        `.config/valet/Certificates/${host}.crt`
+    )
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: { host },
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
